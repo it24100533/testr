a976717b3e3d1f61dcd024464d73e99c586e6c7e
Ravindu Danthanarayana <ravindu.danth@gmail.com>
2025-08-14 21:00:06 +0530

    favicon update final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,48 +21,48 @@ export const metadata: Metadata = {
   description: DATA.description,
   other: {
     "msapplication-TileColor": "#000000",
-    "msapplication-TileImage": "/favicon.png?v=3",
+    "msapplication-TileImage": "favicon.png",
   },
   icons: {
     icon: [
       {
-        url: "/favicon.ico?v=3",
+        url: "favicon.png",
         sizes: "any",
-        type: "image/x-icon",
+        type: "image/png",
       },
       {
-        url: "/favicon.png?v=3",
+        url: "favicon.png",
         sizes: "16x16",
         type: "image/png",
       },
       {
-        url: "/favicon.png?v=3",
+        url: "favicon.png",
         sizes: "32x32",
         type: "image/png",
       },
       {
-        url: "/favicon.png?v=3",
+        url: "favicon.png",
         sizes: "48x48",
         type: "image/png",
       },
       {
-        url: "/favicon.png?v=3",
+        url: "favicon.png",
         sizes: "96x96",
         type: "image/png",
       },
       {
-        url: "/favicon.png?v=3",
+        url: "favicon.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        url: "/favicon.png?v=3",
+        url: "favicon.png",
         sizes: "512x512",
         type: "image/png",
       },
     ],
-    shortcut: "/favicon.ico?v=3",
-    apple: "/favicon.png?v=3",
+    shortcut: "favicon.png",
+    apple: "favicon.png",
   },
   openGraph: {
     title: `${DATA.name}`,
@@ -101,13 +101,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico?v=3" type="image/x-icon" />
-        <link rel="icon" href="/favicon.png?v=3" type="image/png" />
-        <link rel="icon" href="/favicon.png?v=3" type="image/png" sizes="32x32" />
-        <link rel="icon" href="/favicon.png?v=3" type="image/png" sizes="16x16" />
-        <link rel="shortcut icon" href="/favicon.ico?v=3" />
-        <link rel="apple-touch-icon" href="/favicon.png?v=3" />
-        <meta name="msapplication-TileImage" content="/favicon.png?v=3" />
+        <link rel="icon" type="image/png" href="favicon.png" />
+        <link rel="icon" href="favicon.png" type="image/png" />
+        <link rel="icon" href="favicon.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="favicon.png" type="image/png" sizes="16x16" />
+        <link rel="shortcut icon" href="favicon.png" />
+        <link rel="apple-touch-icon" href="favicon.png" />
+        <link rel="icon" href="favicon.png" type="image/x-icon" />
+        <meta name="msapplication-TileImage" content="favicon.png" />
         <meta name="msapplication-TileColor" content="#000000" />
       </head>
       <body
