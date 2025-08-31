b4cd31a47051997a8efe334a5cae287fea5e5e50
Ravindu Danthanarayana <ravindu.danth@gmail.com>
2025-08-31 08:38:35 +0530

    comment unwanted projects

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -183,66 +183,66 @@ export const DATA = {
     }
   ],
   projects: [
-    {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
-      active: true,
-      description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://chatcollect.com",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
-    {
-      title: "Magic UI",
-      href: "https://magicui.design",
-      dates: "June 2023 - Present",
-      active: true,
-      description:
-        "Designed, developed and sold animated UI components for developers.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://magicui.design",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/magicuidesign/magicui",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video: "https://cdn.magicui.design/bento-grid.mp4",
-    },
+    // {
+    //   title: "Chat Collect",
+    //   href: "https://chatcollect.com",
+    //   dates: "Jan 2024 - Feb 2024",
+    //   active: true,
+    //   description:
+    //     "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+    //   technologies: [
+    //     "Next.js",
+    //     "Typescript",
+    //     "PostgreSQL",
+    //     "Prisma",
+    //     "TailwindCSS",
+    //     "Stripe",
+    //     "Shadcn UI",
+    //     "Magic UI",
+    //   ],
+    //   links: [
+    //     {
+    //       type: "Website",
+    //       href: "https://chatcollect.com",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //   ],
+    //   image: "",
+    //   video:
+    //     "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    // },
+    // {
+    //   title: "Magic UI",
+    //   href: "https://magicui.design",
+    //   dates: "June 2023 - Present",
+    //   active: true,
+    //   description:
+    //     "Designed, developed and sold animated UI components for developers.",
+    //   technologies: [
+    //     "Next.js",
+    //     "Typescript",
+    //     "PostgreSQL",
+    //     "Prisma",
+    //     "TailwindCSS",
+    //     "Stripe",
+    //     "Shadcn UI",
+    //     "Magic UI",
+    //   ],
+    //   links: [
+    //     {
+    //       type: "Website",
+    //       href: "https://magicui.design",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //     {
+    //       type: "Source",
+    //       href: "https://github.com/magicuidesign/magicui",
+    //       icon: <Icons.github className="size-3" />,
+    //     },
+    //   ],
+    //   image: "",
+    //   video: "https://cdn.magicui.design/bento-grid.mp4",
+    // },
     {
       title: "Portfolio Website",
       href: "#",
@@ -310,74 +310,74 @@ export const DATA = {
     },
 //see more text
 
-    {
-      title: "project 5",
-      href: "https://automatic.chat",
-      dates: "April 2024 - May 2024",
-      active: true,
-      description:
-        "A simple, student‑friendly GPA calculator built specifically for SLIIT students to help them quickly compute their semester and cumulative GPA accurately  ",
-      technologies: [
-        "Reactjs",
-        "Vite",
-        "Typescript",
-        "ESLint",
-        "npm",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Radix UI",
-        "Vercel",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://sliitgpa.vercel.app/",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/ravindudanthanarayana/gpa",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "/sliitgpa.mov",
-    },
-    {
-      title: "project 6",
-      href: "https://automatic.chat",
-      dates: "April 2024 - May 2024",
-      active: true,
-      description:
-        "A simple, student‑friendly GPA calculator built specifically for SLIIT students to help them quickly compute their semester and cumulative GPA accurately  ",
-      technologies: [
-        "Reactjs",
-        "Vite",
-        "Typescript",
-        "ESLint",
-        "npm",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Radix UI",
-        "Vercel",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://sliitgpa.vercel.app/",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/ravindudanthanarayana/gpa",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "/sliitgpa.mov",
-    },
+    // {
+    //   title: "project 5",
+    //   href: "https://automatic.chat",
+    //   dates: "April 2024 - May 2024",
+    //   active: true,
+    //   description:
+    //     "A simple, student‑friendly GPA calculator built specifically for SLIIT students to help them quickly compute their semester and cumulative GPA accurately  ",
+    //   technologies: [
+    //     "Reactjs",
+    //     "Vite",
+    //     "Typescript",
+    //     "ESLint",
+    //     "npm",
+    //     "TailwindCSS",
+    //     "Shadcn UI",
+    //     "Radix UI",
+    //     "Vercel",
+    //   ],
+    //   links: [
+    //     {
+    //       type: "Website",
+    //       href: "https://sliitgpa.vercel.app/",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //     {
+    //       type: "Source",
+    //       href: "https://github.com/ravindudanthanarayana/gpa",
+    //       icon: <Icons.github className="size-3" />,
+    //     },
+    //   ],
+    //   image: "",
+    //   video:
+    //     "/sliitgpa.mov",
+    // },
+    // {
+    //   title: "project 6",
+    //   href: "https://automatic.chat",
+    //   dates: "April 2024 - May 2024",
+    //   active: true,
+    //   description:
+    //     "A simple, student‑friendly GPA calculator built specifically for SLIIT students to help them quickly compute their semester and cumulative GPA accurately  ",
+    //   technologies: [
+    //     "Reactjs",
+    //     "Vite",
+    //     "Typescript",
+    //     "ESLint",
+    //     "npm",
+    //     "TailwindCSS",
+    //     "Shadcn UI",
+    //     "Radix UI",
+    //     "Vercel",
+    //   ],
+    //   links: [
+    //     {
+    //       type: "Website",
+    //       href: "https://sliitgpa.vercel.app/",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //     {
+    //       type: "Source",
+    //       href: "https://github.com/ravindudanthanarayana/gpa",
+    //       icon: <Icons.github className="size-3" />,
+    //     },
+    //   ],
+    //   image: "",
+    //   video:
+    //     "/sliitgpa.mov",
+    // },
   ],
   hackathons: [
     {
