084eb4ef5b59aac730239885f84f0c6b71aaa25f
Ravindu Danthanarayana <ravindu.danth@gmail.com>
2025-09-10 23:32:59 +0530

    commented out work experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,10 @@ export default function Page() {
           <ProjectsGrid projects={[...DATA.projects]} initialVisibleCount={4} animationDelay={BLUR_FADE_DELAY} />
         </div>
       </section>
-      <section id="work">
+
+
+
+      {/* <section id="work">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 9}>
             <h2 className="text-xl font-bold">Work Experience</h2>
@@ -107,7 +110,12 @@ export default function Page() {
             </BlurFade>
           ))}
         </div>
-      </section>
+      </section> */}
+
+
+
+
+
       <section id="education">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 11}>
