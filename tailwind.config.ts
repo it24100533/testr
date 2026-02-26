8dc576e9127b128c7e6b12f37d7cc3c786c5fb46
ravindudanthanarayana <your_github_email@example.com>
2026-02-26 22:18:26 +0530

    v16.2.3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,12 +97,6 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      padding: {
-        "safe": "env(safe-area-inset-top)",
-        "safe-b": "env(safe-area-inset-bottom)",
-        "safe-l": "env(safe-area-inset-left)",
-        "safe-r": "env(safe-area-inset-right)",
-      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
