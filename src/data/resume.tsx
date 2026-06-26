0145b71553ab87a950464c39fdc144e1346caf31
ravindudanthanarayana <ravindu.danth@gmail.com>
2026-06-26 21:54:36 +0530

    cv updated

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -8,7 +8,7 @@ export const DATA = {
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
     "keeping servers steady & Helping machines learn • Delivering scalable, reliable systems that drive real business impact",
-  cvUrl: "/Ravindu_Danthanarayana_Resume_2026.pdf",
+  cvUrl: "/RAVINDU_DANTHANARAYANA_RESUME_2026_v2.5.pdf",
   summary:
     "Driven **3rd year undergraduate** with a strong passion for **DevOps, Cloud and AI/ML**, who enjoys building smart, reliable and scalable systems. I enjoy learning new technologies, working in team oriented environments and turning ideas into reality. **Currently seeking an internship** where I can learn, contribute and help build meaningful, high impact solutions.",
   avatarUrl: "/ravindu-dp.svg",
